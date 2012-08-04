b91227c9c0e424b166f9be1306717b0c3c7cc578
Robie Lutsey <robie1373@gmail.com>
2012-08-04 02:24:13 -0400

    tests passing. rake and guard both working

--- spec/querinator/query_spec.rb
+++ spec/querinator/query_spec.rb
@@ -1,3 +1,4 @@
+require_relative '../spec_helper'
 module Querinator
   describe Query do
     def setup
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,8 +4,8 @@ require 'minitest/reporters'
 require 'turn'
 require 'growl'
 require_relative '../lib/querinator'
-require_relative 'querinator/importer_spec'
-require_relative 'querinator/query_spec'
+#require_relative 'querinator/importer_spec'
+#require_relative 'querinator/query_spec'
 MiniTest::Reporters.use!
 
 module Querinator
