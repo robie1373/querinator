5574426d5c719954c487671c1def7073726ff887
Robie Lutsey <robie1373@gmail.com>
2012-08-10 11:11:43 -0400

    made it possible to pass in question file location

--- lib/querinator.rb
+++ lib/querinator.rb
@@ -6,8 +6,8 @@ require 'querinator/query'
 module Querinator
   class Game
 
-    def get_questions
-      file_name = "./spec/test_question_file.txt"
+    def get_questions(file_name)
+      file_name ||= "./spec/test_question_file.txt"
       Importer.new.parse(file_name)
     end
   end
